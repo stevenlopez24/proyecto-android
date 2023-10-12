3ff0a8006298534b831d57b383d377e0bbb5a79e
Bryan Lopez G <stivenlopezdator@gmail.com>
2023-10-12 17:15:03 -0500

    Proyecto Final Terminado

--- app/src/main/java/com/example/frutiapp/MainActivity_Nivel1.java
+++ app/src/main/java/com/example/frutiapp/MainActivity_Nivel1.java
@@ -1,10 +1,13 @@
 package com.example.frutiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,14 +21,14 @@ public class MainActivity_Nivel1 extends AppCompatActivity {
     private MediaPlayer mp, mp_great, mp_bad;
     int score, numAleatorio_uno, numAleatorio_dos, resultado, vidas = 3;
     String nombre_jugador, string_score, string_vidas;
-    String numero [] = {"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
+    String numero[] = {"cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nivel1);
 
-        Toast.makeText(this, "Nivel 1 - Sumas básicas", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Nivel 1 - Sumas básicas", Toast.LENGTH_LONG).show();
 
         tv_nombre = (TextView) findViewById(R.id.textView_nombre);
         tv_score = (TextView) findViewById(R.id.textView_score);
@@ -50,31 +53,82 @@ public class MainActivity_Nivel1 extends AppCompatActivity {
         NumAleatorio();
     }
 
-    public void NumAleatorio(){
-        if(score <= 9){
+    public void Comparar(View view) {
+        String respuesta = et_respuesta.getText().toString();
+
+        if (!respuesta.equals("")) {
+
+            int respuesta_jugador = Integer.parseInt(respuesta);
+            if (resultado == respuesta_jugador) {
+
+                mp_great.start();
+                score++;
+                tv_score.setText("Score: " + score);
+                et_respuesta.setText("");
+                BaseDeDatos();
+
+            } else {
+                mp_bad.start();
+                vidas--;
+                BaseDeDatos();
+
+                switch (vidas) {
+                    case 3:
+                        iv_vidas.setImageResource(R.drawable.tresvidas);
+                        break;
+                    case 2:
+                        Toast.makeText(this, "Te quedan 2 manzanas", Toast.LENGTH_LONG).show();
+                        iv_vidas.setImageResource(R.drawable.dosvidas);
+                        break;
+                    case 1:
+                        Toast.makeText(this, "Te queda 1 manzana", Toast.LENGTH_LONG).show();
+                        iv_vidas.setImageResource(R.drawable.unavida);
+                        break;
+                    case 0:
+                        Toast.makeText(this, "Has perdido todas tus manzanas", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                        mp.stop();
+                        mp.release();
+                        break;
+                }
+                et_respuesta.setText("");
+            }
+
+            NumAleatorio();
+
+        } else {
+            Toast.makeText(this, "Escribe tu respuesta", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void NumAleatorio() {
+        if (score <= 9) {
 
             numAleatorio_uno = (int) (Math.random() * 10);
             numAleatorio_dos = (int) (Math.random() * 10);
 
             resultado = numAleatorio_uno + numAleatorio_dos;
 
-            if(resultado <= 10){
+            if (resultado <= 10) {
 
-                for (int i = 0; i < numero.length; i++){
+                for (int i = 0; i < numero.length; i++) {
                     int id = getResources().getIdentifier(numero[i], "drawable", getPackageName());
-                    if(numAleatorio_uno == i){
+                    if (numAleatorio_uno == i) {
                         iv_Auno.setImageResource(id);
 
-                    }if(numAleatorio_dos == i){
+                    }
+                    if (numAleatorio_dos == i) {
                         iv_Ados.setImageResource(id);
                     }
                 }
 
-            }else{
+            } else {
                 NumAleatorio();
             }
 
-        }else{
+        } else {
             Intent intent = new Intent(this, MainActivity_Nivel2.class);
 
             string_score = String.valueOf(score);
@@ -89,4 +143,41 @@ public class MainActivity_Nivel1 extends AppCompatActivity {
             mp.release();
         }
     }
+
+    public void BaseDeDatos(){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "BD", null, 1);
+        SQLiteDatabase BD = admin.getWritableDatabase();
+
+        Cursor consulta = BD.rawQuery("select * from puntaje where score = (select max(score) from puntaje)", null);
+        if(consulta.moveToFirst()){
+            String temp_nombre = consulta.getString(0);
+            String temp_score = consulta.getString(1);
+
+            int bestScore = Integer.parseInt(temp_score);
+
+            if(score > bestScore){
+                ContentValues modificacion = new ContentValues();
+                modificacion.put("nombre", nombre_jugador);
+                modificacion.put("score", score);
+
+                BD.update("puntaje", modificacion, "score=" + bestScore, null);
+            }
+
+            BD.close();
+
+        }else{
+            ContentValues insertar = new ContentValues();
+            insertar.put("nombre", nombre_jugador);
+            insertar.put("score", score);
+
+            BD.insert("puntaje", null, insertar);
+            BD.close();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
 }
