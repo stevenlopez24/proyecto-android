e039afc9df8f5e420c537ff272587dd9ccbee2cf
BryanLopez <stivenlopezdator@gmail.com>
2023-10-27 10:43:19 -0500

    Final

--- Proyecto FrutiApp/app/src/main/java/com/example/frutiapp/MainActivity.java
+++ Proyecto FrutiApp/app/src/main/java/com/example/frutiapp/MainActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private EditText et_nombre;
     private ImageView iv_personaje;
     private TextView tv_bestScore;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("pruba");
+
         et_nombre = (EditText) findViewById(R.id.txt_nombre);
         iv_personaje = (ImageView) findViewById(R.id.imageView_Personaje);
         tv_bestScore = (TextView) findViewById(R.id.textView_BestScore);
@@ -37,19 +38,19 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
         int id;
-        if (num_aleatorio == 0 || num_aleatorio == 10) {
+        if(num_aleatorio == 0 || num_aleatorio == 10){
             id = getResources().getIdentifier("mango", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
-        } else if (num_aleatorio == 1 || num_aleatorio == 9) {
+        }else if(num_aleatorio == 1 || num_aleatorio == 9){
             id = getResources().getIdentifier("fresa", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
-        } else if (num_aleatorio == 2 || num_aleatorio == 8) {
+        }else if(num_aleatorio == 2 || num_aleatorio == 8){
             id = getResources().getIdentifier("manzana", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
-        } else if (num_aleatorio == 3 || num_aleatorio == 7) {
+        }else if(num_aleatorio == 3 || num_aleatorio == 7){
             id = getResources().getIdentifier("sandia", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
-        } else if (num_aleatorio == 4 || num_aleatorio == 5 || num_aleatorio == 6) {
+        }else if(num_aleatorio == 4 || num_aleatorio == 5 || num_aleatorio == 6){
             id = getResources().getIdentifier("uva", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
         }
@@ -60,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         Cursor consulta = BD.rawQuery(
                 "select * from puntaje where score = (select max(score) from puntaje)", null);
 
-        if (consulta.moveToFirst()) {
+        if(consulta.moveToFirst()){
             String temp_nombre = consulta.getString(0);
             String temp_score = consulta.getString(1);
             tv_bestScore.setText("Record: " + temp_score + " de " + temp_nombre);
@@ -75,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void Jugar(View view) {
+    public void Jugar(View view){
         String nombre = et_nombre.getText().toString();
 
-        if (!nombre.equals("")) {
+        if(!nombre.equals("")){
             mp.stop();
             mp.release();
 
@@ -87,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra("Jugador", nombre);
             startActivity(intent);
             finish();
-        } else {
+        }else{
             Button btn_jugar = findViewById(R.id.btn_jugar);
             btn_jugar.setEnabled(false);
 
@@ -100,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
             }, Toast.LENGTH_LONG + 1500);
 
             et_nombre.requestFocus();
-            InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
             imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
         }
     }
